2943b4aa53ebdcc6ff412b551f024da7aaf8bb7f
Eric Njogu <kunadawa@gmail.com>
2019-10-30 17:19:18 +0300

    fix: removing mock after discovering cv2 error was thrown for real data

--- visualize_image_tests.py
+++ visualize_image_tests.py
@@ -16,9 +16,7 @@ def test_create_handle_detection_request(create_handle_detection_request):
     handler.video_writer = mock.Mock()
     handler.category_index = category_index
     handler.output_video_file_path = mock.Mock()
-    # https://medium.com/@yeraydiazdiaz/what-the-mock-cheatsheet-mocking-in-python-6a71db997832
-    with mock.patch('visualize_image.cv2') as mocked_cv2:
-        handler.handle_detection(msg, mock.Mock())
+    handler.handle_detection(msg, mock.Mock())
 
     # assertions
     handler.video_writer.write.assert_called_once()
