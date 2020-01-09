2366816661aa62802daa64a65189a6762e317657
Eric Njogu <kunadawa@gmail.com>
2020-01-09 08:10:16 +0300

    feat: stream a video to rtp
    This is a proof of concept that we will attempt to apply to frames where 
    bounding boxes have been visualized

---
+++ rtp_stream.py
@@ -0,0 +1,30 @@
+import imageio
+import os
+import imageio_ffmpeg
+
+def write_video_to_stream(path_to_video, rtp_port):
+    video_reader = imageio.get_reader(path_to_video)
+    img_writer = None
+    rtp_url = f"rtp://localhost:{rtp_port}"
+    output_params = [ '-f', 'rtp_mpegts']
+    input_params = ['-re']
+    for frame in video_reader:
+        if img_writer is None:
+            img_writer = imageio_ffmpeg.write_frames(
+                    rtp_url,
+                    size=(frame.shape[1], frame.shape[0]),
+                    output_params=output_params,
+                    input_params=input_params,
+                    fps=10,
+                    )
+            img_writer.send(None)
+        img_writer.send(frame)
+    img_writer.close()
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path_to_video")
+    parser.add_argument("rtp_port")
+    ns = parser.parse_args()
+    write_video_to_stream(ns.path_to_video, ns.rtp_port)
