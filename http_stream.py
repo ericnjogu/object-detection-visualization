81a4851186244309441b5a056c64d9577634c2b8
Eric Njogu <kunadawa@gmail.com>
2020-01-10 09:45:03 +0300

    feat: streaming webcam over http
    Experienced more latency as compared to rtp (up to 2 seconds more than 
    rtp which was about 3 seconds)

---
+++ http_stream.py
@@ -0,0 +1,34 @@
+import imageio
+import os
+import imageio_ffmpeg
+
+def write_video_to_stream(path_to_video, rtp_port):
+    video_reader = imageio.get_reader(path_to_video)
+    print(video_reader.get_meta_data())
+    rtp_url = f"http://localhost:{rtp_port}"
+    output_params = [ '-f', 'Ogg', '-listen', "1", '-seekable', '0']
+    input_params = ['-re']
+    img_writer = imageio_ffmpeg.write_frames(
+            rtp_url,
+            size=video_reader.get_meta_data()['size'],
+            output_params=output_params,
+            input_params=input_params,
+            codec='theora',
+    )
+    img_writer.send(None)
+    for frame in video_reader:
+        try:
+            img_writer.send(frame)
+        except KeyboardInterrupt as e:
+            img_writer.close()
+            break
+    img_writer.close()
+
+# python http_stream.py "<video0>" 50644
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path_to_video")
+    parser.add_argument("rtp_port")
+    ns = parser.parse_args()
+    write_video_to_stream(ns.path_to_video, ns.rtp_port)
