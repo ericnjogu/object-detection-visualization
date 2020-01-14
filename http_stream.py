6b2c5874a830bc304c40aaf1cf56662c7cb8cd73
Eric Njogu <kunadawa@gmail.com>
2020-01-14 15:03:56 +0300

    fix: send CORS header, use compatible HTML codecs

--- http_stream.py
+++ http_stream.py
@@ -6,22 +6,24 @@ def write_video_to_stream(path_to_video, rtp_port):
     video_reader = imageio.get_reader(path_to_video)
     print(video_reader.get_meta_data())
     rtp_url = f"http://localhost:{rtp_port}"
-    output_params = [ '-f', 'Ogg', '-listen', "1", '-seekable', '0']
+    output_params = [ '-f', 'webm', '-listen', "1", '-seekable', '0',
+        '-headers', 'Access-Control-Allow-Origin: http://localhost:3000\r\n']
     input_params = ['-re']
     img_writer = imageio_ffmpeg.write_frames(
             rtp_url,
             size=video_reader.get_meta_data()['size'],
             output_params=output_params,
             input_params=input_params,
-            codec='theora',
+            codec='vp9',
     )
     img_writer.send(None)
     for frame in video_reader:
-        try:
+        #try:
             img_writer.send(frame)
-        except KeyboardInterrupt as e:
-            img_writer.close()
-            break
+        #except KeyboardInterrupt as e:
+        #    print(e)
+        #    img_writer.close()
+        #    break
     img_writer.close()
 
 # python http_stream.py "<video0>" 50644
